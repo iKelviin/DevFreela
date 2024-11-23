b7df84c68e01bd1e46610a34a4a46989c47c04cf
Kelvin Rocha <101436562+iKelviin@users.noreply.github.com>
2024-11-23 16:31:35 -0300

    controllers, models and database in memory

--- DevFreela.API/Controllers/ProjectsController.cs
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -1,6 +1,9 @@
-﻿using DevFreela.API.Models;
+﻿using DevFreela.API.Entities;
+using DevFreela.API.Models;
+using DevFreela.API.Persistence;
 using DevFreela.API.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 
 namespace DevFreela.API.Controllers
@@ -9,23 +12,38 @@ namespace DevFreela.API.Controllers
     [Route("api/projects")]
     public class ProjectsController : ControllerBase
     {        
-        public ProjectsController()
+        private readonly DevFreelaDbContext _context;
+        public ProjectsController(DevFreelaDbContext context)
         {
-
+            _context = context;
         }
 
         // GET api/projects?search=crm
         [HttpGet]
         public IActionResult Get(string search = "")
         {
-            return Ok();
+            var projects = _context.Projects
+                            .Include(p => p.Client)
+                            .Include(p => p.Freelancer)
+                            .Where(p => !p.IsDeleted).ToList();
+
+            var model = projects.Select(ProjectItemViewModel.FromEntity).ToList();
+
+            return Ok(model);
         }
 
         // GET api/projects/1234
         [HttpGet("{id}")]
         public IActionResult GetById(int id)
         {
-            return Ok();
+            var project =_context.Projects
+                            .Include(p => p.Client)
+                            .Include(p => p.Freelancer)
+                            .Include(p => p.Comments)
+                            .SingleOrDefault(p => p.Id == id);
+
+            var model = ProjectViewModel.FromEntity(project);
+            return Ok(model);
 
         }
 
@@ -33,6 +51,11 @@ namespace DevFreela.API.Controllers
         [HttpPost]
         public IActionResult Post(CreateProjectInputModel model)
         {
+            var project = model.ToEntity();
+
+            _context.Projects.Add(project);
+            _context.SaveChanges();
+
             return CreatedAtAction(nameof(GetById), new { id = 1 }, model);
         }
 
@@ -40,6 +63,15 @@ namespace DevFreela.API.Controllers
         [HttpPut("{id}")]
         public IActionResult Put(int id, UpdateProjectInputModel model)
         {
+            var project = _context.Projects.SingleOrDefault(p => p.Id == id);
+
+            if (project == null) return NotFound();
+
+            project.Update(model.Title, model.Description, model.TotalCost);
+
+            _context.Projects.Update(project);
+            _context.SaveChanges();
+
             return NoContent();
         }
 
@@ -47,6 +79,14 @@ namespace DevFreela.API.Controllers
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
+            var project = _context.Projects.SingleOrDefault(p => p.Id == id);
+
+            if (project == null) return NotFound();
+
+            project.SetAsDeleted();
+            _context.Projects.Update(project);
+            _context.SaveChanges();
+
             return NoContent();
         }
 
@@ -54,6 +94,14 @@ namespace DevFreela.API.Controllers
         [HttpPut("{id}/start")]
         public IActionResult Start(int id)
         {
+            var project = _context.Projects.SingleOrDefault(p => p.Id == id);
+
+            if (project == null) return NotFound();
+
+            project.Start();
+            _context.Projects.Update(project);
+            _context.SaveChanges();
+
             return NoContent();
         }
 
@@ -61,6 +109,14 @@ namespace DevFreela.API.Controllers
         [HttpPut("{id}/complete")]
         public IActionResult Complete(int id)
         {
+            var project = _context.Projects.SingleOrDefault(p => p.Id == id);
+
+            if (project == null) return NotFound();
+
+            project.Complete();
+            _context.Projects.Update(project);
+            _context.SaveChanges();
+
             return NoContent();
         }
 
@@ -68,6 +124,15 @@ namespace DevFreela.API.Controllers
         [HttpPost("{id}/comments")]
         public IActionResult PostComment(int id, CreateProjectCommentInputModel model)
         {
+            var project = _context.Projects.SingleOrDefault(p => p.Id == id);
+
+            if (project == null) return NotFound();
+
+            var comment = new ProjectComment(model.Content, model.IdProject, model.IdUser);
+
+            _context.ProjectComments.Add(comment);
+            _context.SaveChanges();
+
             return Ok();
         }
     }
--- DevFreela.API/Models/ProjectItemViewModel.cs
+++ DevFreela.API/Models/ProjectItemViewModel.cs
@@ -19,5 +19,11 @@ namespace DevFreela.API.Models
         public string ClientName { get; private set; }
         public string FreelancerName { get; private set; }
         public decimal TotalCost { get; private set; }
+
+        public static ProjectItemViewModel FromEntity(Project project)
+        {
+            return new(project.Id, project.Title, project.Client.FullName, project.Freelancer.FullName, project.TotalCost);
+        }
+
     }
 }
